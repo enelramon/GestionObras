c374bdfd5f2f078bd05aeac5194b18f1c6a448df
JoseangelVargaspolaco <jose_vargas7@ucne.edu.do>
2023-03-23 21:30:49 -0400

    Agregando el controllador de persona en GestionObras.Apo #2

--- GestionObras.Api/DAL/Contexto.cs
+++ GestionObras.Api/DAL/Contexto.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GestionObras.Api.DAL
 {
-    public class Contexto:DbContext
+    public class Contexto : DbContext
     {
-        public Contexto(DbContextOptions<Contexto> options): base(options) 
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
         {
 
         }
@@ -18,12 +18,38 @@ namespace GestionObras.Api.DAL
         {
             base.OnModelCreating(modelBuilder);
             //modelBuilder.Entity<Proyectos>().HasData(new Proyectos { });
-            modelBuilder.Entity<Personas>().HasData(new Personas {
-                PersonaId = 1,
-                Nombres = "Manuel",
-                Telefono = "829-811-4569"
-             });
-            
+            modelBuilder.Entity<Personas>().HasData(
+               new Personas
+               {
+                   PersonaId = 1,
+                   Nombres = "Manuel",
+                   Telefono = "829-811-4569"
+               },
+               new Personas
+               {
+                   PersonaId = 2,
+                   Nombres = "Samuel",
+                   Telefono = "829-846-5619"
+               },
+               new Personas
+               {
+                   PersonaId = 3,
+                   Nombres = "Juan",
+                   Telefono = "809-578-1978"
+               },
+               new Personas
+               {
+                   PersonaId = 4,
+                   Nombres = "Ana",
+                   Telefono = "849-678-6719"
+               },
+               new Personas
+               {
+                   PersonaId = 5,
+                   Nombres = "Josefa",
+                   Telefono = "849-789-1290"
+               }
+             );
         }
     }
 }
